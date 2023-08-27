7bf0bf356c12dd04d9e9fc245774d5b0a58bd5b5
Arden Ranta <arranta20@gmail.com>
2023-08-26 19:34:01 -0700

    Pass first 7 tests in order.js.

--- src/order.js
+++ src/order.js
@@ -1,7 +1,22 @@
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
+var deliveryOrders = []
+function takeOrder(order, destList) {
+  if (destList.length < 3) {
+  return destList.push(order);
+  }
+}
+
+function refundOrder(orderNum, array) {
+  for (i = 0; i < array.length; i++)
+  {
+  if (array[i].orderNumber === orderNum) {
+    array.splice(i, 1);
+  }
+} return array;
+} 
