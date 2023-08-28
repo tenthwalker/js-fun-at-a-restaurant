6ec6db93e4e34f6634a51bb3caba78ccb2ff5ccb
Arden Ranta <arranta20@gmail.com>
2023-08-27 18:07:08 -0700

    Add the first 10 passing tests for restaurant.js.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,27 @@
+function createRestaurant(name, menus) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+
+function addMenuItem(restaurant, item) {
+ var menuTypes = restaurant.menus[item.type];
+  for (i = 0; i < menuTypes.length; i++){
+   if (menuTypes[i].name === item.name) {
+     return ;
+   } 
+ } return menuTypes.push(item);
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
